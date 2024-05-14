288f3c317b9e18fed12af57de8ab32b107b3250a
nasserboan <nasserboan@gmail.com>
2024-05-14 14:37:47 -0300

    finish

--- src/data_ingestion/pipeline.py
+++ src/data_ingestion/pipeline.py
@@ -3,21 +3,44 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-import chromadb
 
+import chromadb
 from dotenv import load_dotenv
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def load_and_split(splitter, path):
-    
+    """
+    Load a PDF document from the given path and split it into an array of documents using the specified splitter.
+
+    Args:
+        splitter (TextSplitter): The splitter object used to split the text content of the PDF document.
+        path (str): The path to the PDF document.
+
+    Returns:
+        list: An array of chunk objects, each containing a chunk of the original document.
+
+    """
     loader = PyPDFLoader(path)
     doc_array = loader.load_and_split(text_splitter=splitter)
 
     return doc_array
 
 def init_splitter(chunk, overlap):
-    
+    """
+    Initialize and return a RecursiveCharacterTextSplitter object.
+
+    Parameters:
+    - chunk (int): The size of each chunk.
+    - overlap (int): The overlap between consecutive chunks.
+
+    Returns:
+    - splitter (RecursiveCharacterTextSplitter): The initialized splitter object.
+    """
+
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk,
         chunk_overlap=overlap,
@@ -28,11 +51,37 @@ def init_splitter(chunk, overlap):
     return splitter
 
 def init_embedding_model(model_name):
-    embedding_model = OpenAIEmbeddings(model=model_name)
+    """
+    Initializes an embedding model using the specified model name.
+
+    Args:
+        model_name (str): The name of the embedding model to initialize.
+
+    Returns:
+        embedding_model: The initialized embedding model.
+
+    """
+    embedding_model = OpenAIEmbeddings(model=model_name)    
     return embedding_model
 
 def init_connection_vectordb(host, port, collection, model):
-    c_client = chromadb.HttpClient(host, port)
+    """
+    Initializes a connection to VectorDB and creates a collection if it doesn't exist.
+
+    Args:
+        host (str): The host address of the VectorDB server.
+        port (int): The port number of the VectorDB server.
+        collection (str): The name of the collection to be created.
+        model: The embedding function to be used for the collection.
+
+    Returns:
+        Chroma: An instance of the Chroma class representing the connection to VectorDB.
+
+    Raises:
+        ValueError: If the specified collection already exists.
+
+    """
+    c_client = chromadb.HttpClient(host=host, port=port)
     
     if collection in c_client.list_collections():
         raise ValueError(f"Collection {collection} already exists. Please delete it or use a different name.")
@@ -43,6 +92,9 @@ def init_connection_vectordb(host, port, collection, model):
     return db
     
 if __name__ == "__main__":
+
+    
+
     load_dotenv()
 
     splitter = init_splitter(1000, 100)
@@ -50,15 +102,16 @@ if __name__ == "__main__":
     embedding_model = init_embedding_model(os.getenv("MODEL_NAME"))
     
     db = init_connection_vectordb(os.getenv("CHROMA_HOST"), 
-                                  os.getenv("CHROMA_PORT"), 
-                                  os.getenv("COLLECTION_NAME"), 
-                                  model=embedding_model)
+                                    os.getenv("CHROMA_PORT"), 
+                                    os.getenv("COLLECTION_NAME"), 
+                                    model=embedding_model)
     
     for doc in glob.glob("docs/*.pdf"):
         
+        logging.info("Iniciando o processamento do documento: %s", doc)
         splitted_docs = load_and_split(splitter, doc)
         
         _ = db.add_documents(splitted_docs)
-        print('acabei')
+        logging.info("Finalizado o processamento do documento: %s", doc)
 
     
